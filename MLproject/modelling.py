110a57c4ce464bde2bcdea0a3ae3135c413f2e29
Angga Zakariya <anggazakaria31@gmail.com>
2025-12-18 11:22:31 +0700

    Y GEDE

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -26,9 +26,8 @@ if __name__ == "__main__":
     n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 505
     max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 37
 
-    run_id = mlflow.active_run().info.run_id if mlflow.active_run() else None
 
-    with mlflow.start_run(run_id=run_id):
+    with mlflow.start_run(nested=True):
         model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
         model.fit(X_train, y_train)
 
